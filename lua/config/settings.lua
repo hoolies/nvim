6b2a7457658107a6a860c77d71d4b71176823a41
hoolies <c.rouvellas@gmail.com>
2024-04-13 19:29:55 -0400

    Fix the comments

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,21 +1,21 @@
 ----------------------------------=={ Setup Local }==-------------------------------
-local opt = vim.wo                                    -- Set opt as vim.wo, cause lazy again
-local set = vim.opt		                                -- Set set as vim.opt, cause lazy
+local opt = vim.wo                                     -- Set opt as vim.wo, cause lazy again
+local set = vim.opt		                       -- Set set as vim.opt, cause lazy
 
 
 ----------------------------------=={  Settings   }==-------------------------------
 
 
 -- Behavior
-set.autoindent = true                                   -- Auto indent lines
+set.autoindent = true                                  -- Auto indent lines
 set.scrolloff = 5
 set.showmatch = true
 
 
 -- Buffer
-set.autoread = true                                     -- Automatically update file
-set.hidden = true                                       -- Enable background buffers
-set.updatetime = 250                                    -- Automatically update 
+set.autoread = true                                    -- Automatically update file
+set.hidden = true                                      -- Enable background buffers
+set.updatetime = 250                                   -- Automatically update 
 
 
 -- Copy to clipboard
@@ -28,25 +28,25 @@ set.cursorline = true
 
 
 -- Highlights
-vim.cmd[[highlight LineNr guifg=#29465B]]               -- The Numbers on the left
-vim.cmd[[highlight CursorColumn guifg=#737CA1]]         -- The cursorcolumn color
-vim.cmd[[highlight CursorLine guifg=#737CA1]]           -- The cursorline color
-vim.cmd[[highlight CursorLineNr guifg=#FF00FF]]         -- The number of actual line
+vim.cmd[[highlight LineNr guifg=#29465B]]              -- The Numbers on the left
+vim.cmd[[highlight CursorColumn guifg=#737CA1]]        -- The cursorcolumn color
+vim.cmd[[highlight CursorLine guifg=#737CA1]]          -- The cursorline color
+vim.cmd[[highlight CursorLineNr guifg=#FF00FF]]        -- The number of actual line
 
 
 -- Line wraps
 set.wrapscan = true
-opt.wrap = true                                      -- Wrap the line
+opt.wrap = true                               	       -- Wrap the line
 opt.linebreak = true
 opt.list = false
 
 
 -- Number column
-set.number = true	        	                            -- Set the number
+set.number = true	        	                -- Set the number
 set.numberwidth = 5                                     -- Width of the number on the left
-set.relativenumber = true	                              -- Set relative numbers
+set.relativenumber = true	                        -- Set relative numbers
 set.ruler = true                                        --
-opt.colorcolumn = '0'                                --
+opt.colorcolumn = '0'                                	--
 
 
 -- Search
@@ -58,7 +58,7 @@ set.incsearch = true                                    -- Set incremental
 set.expandtab = true                                    -- Use Spaces instead of tabs
 set.shiftwidth = 4                                      -- Round Indent
 set.softtabstop = 4
-set.tabstop = 4     		      	                        -- Tab space
+set.tabstop = 4     		      	                -- Tab space
 
 
 -- Undo history settings
