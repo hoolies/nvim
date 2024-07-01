70061f41dfd66bb7f82f9ecef8df3045228de65b
hoolies <c.rouvellas@gmail.com>
2024-06-30 21:22:40 -0400

    Setting up NVIM-CMP

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,19 @@
+return {
+  'mfussenegger/nvim-lint',
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require('lint')
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      terraform =  { "tflint"},
+      python = { "flake8" },
+    }
+
+  end
+
+}
