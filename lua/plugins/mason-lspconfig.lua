eaea397b2fcbef057851b432d1c098ac9446a539
hoolies <c.rouvellas@gmail.com>
2025-02-04 21:58:25 -0500

    Changing language servers

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,27 +1,34 @@
-local opts = {
-	ensure_installed = {
-		"bashls",
-    "black",
-		"cssls",
-		"efm",
-    "elixirls",
-		"emmet_ls",
-    "flake8",
-		"html",
-		"jsonls",
-		"lua_ls",
-		"pyright",
-		"solidity",
-		"tailwindcss",
-		"ts_ls",
-	},
-
-	automatic_installation = true,
-}
-
 return {
-	"williamboman/mason-lspconfig.nvim",
-	opts = opts,
-	event = "BufReadPre",
-	dependencies = "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  config = function()
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        "ansiblels",
+        "bashls",
+        "clangd",
+        "cssls",
+        "dockerls",
+        "efm",
+        "elixirls",
+        "emmet_ls",
+        "gopls",
+        "grammarly",
+        "html",
+        "jinja_lsp",
+        "jsonls",
+        "kotlin_language_server",
+        "lemminx",
+        "lua_ls",
+        "nginx_language_server",
+        "ruff",
+        "rust_analyzer",
+        "solidity",
+        "sqlls",
+        "tailwindcss",
+        "terraformls",
+        -- "pyright",
+      },
+    })
+  end,
+  automatic_installation = true,
 }
